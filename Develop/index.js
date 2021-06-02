adea9764adb71b97e8794882782c9d95bcdaf584
Kimberly Salas <salask0104@gmail.com>
2021-06-02 06:18:24 -0700

    added questions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,37 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ()=> {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'Required: What is the name of your project?',
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the project name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Required: Please provide a description og your project.',
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the project name!');
+                    return false;
+                }
+            }
+        },
+
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
