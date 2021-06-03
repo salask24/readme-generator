84c560ef9c420fa03c283f206d857d7964de5bf8
Kimberly Salas <salask0104@gmail.com>
2021-06-02 20:10:58 -0700

    edited index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const questions = () => {
         {
             type: 'input',
             name: 'projectName',
-            message: 'Required: What is the name of your project?',
+            message: 'What is the name of the project? (Required)',
             validate: answerInput => {
                 if (answerInput) {
                     return true;
@@ -22,12 +22,12 @@ const questions = () => {
         {
             type: 'input',
             name: 'description',
-            message: 'Required: Please provide a description of your project.',
+            message: 'Provide a description of the project. (Required)',
             validate: answerInput => {
                 if (answerInput) {
                     return true;
                 } else {
-                    console.log('Please enter a description of tour project!');
+                    console.log('Please enter a description!');
                     return false;
                 }
             }
@@ -35,32 +35,172 @@ const questions = () => {
         {
             type: 'confirm',
             name: 'confirmTableOfContents',
-            message: 'Optional: Would you like to include a table of contents?',
+            message: 'Would you like to add a Table of Contents?',
             default: true
         },
         {
-            type: "checkbox",
-            name: "license",
-            message: "Please choose a license",
-            choices: ['MIT', 'Apache License 2.0', 'ISC', 'GNU GPLv2']
+            type: 'input',
+            name: 'installation',
+            message: 'Provide the installation instructions for the project. (Required)',
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter install instructions!');
+                    return false;
+                }
+            }
         },
         {
-            type: "input",
-            name: "tests",
-            message: "Please enter information on testing",
+            type: 'input',
+            name: 'usage',
+            message: 'Provide content for the usage of the project. (Required)',
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter usage content!');
+                    return false;
+                }
+            }
         },
         {
-            type: "input",
-            name: "github",
-            message: "Please enter your GitHub username"
+            type: 'input',
+            name: 'credits',
+            message: 'Enter any credits for the project. (Required)',
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter credits for the project!');
+                    return false;
+                }
+            }
         },
         {
-            type: "input",
-            name: "email",
-            message: "Please enter your email address"
+            type: 'confirm',
+            name: 'confirmLicense',
+            message: 'Would you like to add a License?',
+            default: false
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Select the license for this project.',
+            choices: ['MIT License', 'GNU General Public License v3.0', 'Mozilla Public License 2.0', 'The Unlicense'],
+            when: ({ confirmLicense }) => {
+                if (confirmLicense) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmBadges',
+            message: 'Would you like to add any Badges?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'badges',
+            message: 'Enter any Badges you would like to add.',
+            when: ({ confirmBadges }) => {
+                if (confirmBadges) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmFeatures',
+            message: 'Would you like list any features?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'List any features of the project.',
+            when: ({ confirmFeatures }) => {
+                if (confirmFeatures) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmContributing',
+            message: 'Would you like to provide contributing information?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Provide any information for contributing to the project.',
+            when: ({ confirmContributing }) => {
+                if (confirmContributing) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmTests',
+            message: 'Would you like to add any Tests?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide any information on tests for the project',
+            when: ({ confirmTests }) => {
+                if (confirmTests) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username? (Required)',
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your contact e-mail address? (Required)',
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your contact email!');
+                    return false;
+                }
+            }
         }
     ])
-}
+};
 
 questions()
     .then(answers => {
